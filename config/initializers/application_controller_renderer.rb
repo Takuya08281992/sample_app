f02e06060ac479ba49e4d4196f6034760876a511
山崎拓也 <yamazakitakuya@yamazakitakuyanoMacBook-Pro.local>
2020-05-06 18:44:13 +0900

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '613b9588e15874eb813e2b7e9fe3c93b30984371c76251252a23d77cc7762de5f1483f6a48c3a285b70eaebc8e2659cdcb6f1ba37d1569a28cda9ff20bf5ea86'
